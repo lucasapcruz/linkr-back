78114c2f893d0a2fb14eeb297e67d5bd1726cad6
AdrienneMota <adriennemota97@gmail.com>
2023-01-04 16:44:01 -0400

    feat: signin and signup

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import postsRoutes from './routes/posts.routes.js';
+import authRoutes from './routes/user.routes.js';
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(postsRoutes);
+app.use(authRoutes)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running in port ${PORT}`));
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,10 +1,11 @@
 import Joi from "joi";
 
-export const userSchema = Joi.object({
+const userSchema = Joi.object({
     "name":Joi.string().min(4).required(),
     "email":Joi.string().email().required(),
     "password":Joi.string().min(3).trim(false).required(),
     "image_url": Joi.string().uri().required()
 })
 
+export default userSchema
 
