a72efb8e51289e12f125f03d41be99fb54e45f0e
iamandreribeiro <andreribeiroorafael@gmail.com>
2023-01-04 20:11:34 -0300

    feat: liking & disliking posts

--- src/controllers/posts.controllers.js
+++ src/controllers/posts.controllers.js
@@ -1,35 +1,43 @@
 import { connection } from "../database/server.js";
 import { getLinkPreview, getPreviewFromContent } from "link-preview-js";
 
-export async function createPost (req, res) {
+export async function createPost(req, res) {
   const { link, message } = res.locals.data;
 
   // Filter Hashtags
   const words = message.split(" ");
-  const hashtags = words.filter(word => word[0] === "#")
-  .map(word => word.replace("#", ""));
-  
+  const hashtags = words
+    .filter((word) => word[0] === "#")
+    .map((word) => word.replace("#", ""));
+
   try {
     // Token > User
-    const queryUser = await connection.query(`
+    const queryUser = await connection.query(
+      `
       SELECT u.id FROM users AS u
       JOIN sessions AS s ON u.id = s.user_id
       WHERE s.token = $1
-    `, [res.locals.token]);
+    `,
+      [res.locals.token]
+    );
     const userId = queryUser.rows[0].id;
 
     // Post
-    const queryPost = await connection.query(`
-      INSERT INTO posts (user_id, link, message) VALUES ($1, $2, $3) RETURNING id`
-    , [userId, link ? link : "", message]);
-    
+    const queryPost = await connection.query(
+      `
+      INSERT INTO posts (user_id, link, message) VALUES ($1, $2, $3) RETURNING id`,
+      [userId, link ? link : "", message]
+    );
+
     // Hashtags
     const postId = queryPost.rows[0].id;
     for (let i = 0; i < hashtags.length; i++) {
       const name = hashtags[i];
-      await connection.query(`
-        INSERT INTO hashtags (name, post_id) VALUES ($1, $2)`
-      , [name, postId]);
+      await connection.query(
+        `
+        INSERT INTO hashtags (name, post_id) VALUES ($1, $2)`,
+        [name, postId]
+      );
     }
 
     res.sendStatus(201);
@@ -37,10 +45,9 @@ export async function createPost (req, res) {
     console.log(err.message);
     res.sendStatus(500);
   }
-};
+}
 
-export async function getPosts (req, res) {
-
+export async function getPosts(req, res) {
   try {
     const queryPosts = await connection.query(`
       SELECT p.id, u.image_url, u.name, p.link, p.message FROM posts AS p
@@ -53,19 +60,57 @@ export async function getPosts (req, res) {
     for (let i = 0; i < posts.length; i++) {
       const e = posts[i];
       if (e.link) {
-        const { title, description, url, images, favicons } = await getLinkPreview(e.link);
-        e.link = { 
+        const { title, description, url, images, favicons } =
+          await getLinkPreview(e.link);
+        e.link = {
           title,
           description,
           url,
-          image: images.length ? images[0] : favicons[0]
+          image: images.length ? images[0] : favicons[0],
         };
       }
     }
-    
+
     res.send(posts);
   } catch (err) {
     console.log(err.message);
     res.sendStatus(500);
   }
-};
+}
+
+export async function likePost(req, res) {
+  const { postId } = req.body;
+
+  const queryUser = await connection.query(
+    `SELECT u.id 
+    FROM users AS u 
+    JOIN sessions AS s 
+    ON u.id = s.user_id 
+    WHERE s.token = $1`,
+    [res.locals.token]
+  );
+
+  const userId = queryUser.rows[0].id;
+
+  const queryLikes = await connection.query(
+    `SELECT *
+    FROM likes
+    WHERE user_id=$1 AND post_id=$2;`,
+    [userId, postId]
+  );
+
+  if (queryLikes.rows.length > 0) {
+    console.log(userId);
+    await connection.query(
+      `DELETE FROM likes
+      WHERE user_id=$1 AND post_id=$2;`,
+      [userId, postId]
+    );
+  } else {
+    await connection.query(
+      `INSERT INTO likes (post_id, user_id)
+      VALUES ($1, $2);`,
+      [postId, userId]
+    );
+  }
+}
