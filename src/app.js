f260cab14772a3e0be25f62d19ca1e37cc774efc
Lucas Cruz <lucas.apcruz@gmail.com>
2023-01-04 15:16:40 -0300

    feat: get trending hashtags route

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import postsRoutes from './routes/posts.routes.js';
+import hashtagsRouter from "./routes/hashtags.routes.js";
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(postsRoutes);
+app.use(hashtagsRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running in port ${PORT}`));
