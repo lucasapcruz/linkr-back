0fc02ec55ea06fc8b49dccde1bbeb0fe52d9373a
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-01-05 16:54:53 -0300

    feat: get user info

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { Signin, Signup } from "../controllers/user.controllers.js";
+import { find, Signin, Signup } from "../controllers/user.controllers.js";
 import { validateSchema } from "../middlewares/schemaValidation.middleware.js";
+import { tokenValidation } from "../middlewares/token.middleware.js";
 import loginSchema from "../models/login.model.js";
 import userSchema from "../models/user.model.js";
 
@@ -8,5 +9,6 @@ const router = Router();
 
 router.post("/sign-up", validateSchema(userSchema), Signup);
 router.post("/sign-in", validateSchema(loginSchema), Signin);
+router.get("/user/:id", tokenValidation, find);
 
 export default router;
