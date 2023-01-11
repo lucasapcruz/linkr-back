a63876d772db127fe743f77b0e9dad821e4079ea
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-01-10 23:40:31 -0300

    feat: repost info when get posts by hashtag

--- src/controllers/posts.controllers.js
+++ src/controllers/posts.controllers.js
@@ -49,7 +49,7 @@ export async function createPost(req, res) {
 }
 
 export async function getPosts(req, res) {
-  const { token } = res.locals;
+  const { token, userInfo } = res.locals;
   const { hashtag } = req.query;
   const { id } = req.params;
 
@@ -139,16 +139,56 @@ export async function getPosts(req, res) {
     } else if (hashtag) {
       queryPosts = await connection.query(
         `
-        SELECT p.id, u.id AS user_id, u.image_url, u.name, p.link, p.message FROM posts AS p
-        JOIN users AS u ON p.user_id = u.id
-        WHERE p.id IN (
-          SELECT h.post_id 
-          FROM hashtags h
-          WHERE h."name" = $1)
-        ORDER BY p.date DESC
-        LIMIT 20
+        WITH tablejoin AS (
+          SELECT
+          p.id,
+          p.user_id,
+          P.message,
+          p.link,
+            r.user_id AS sharer_id,
+          p.date,
+            r.date AS repost_date,
+			    u,name AS sharer_name
+          FROM posts p
+          LEFT JOIN reposts r
+          ON p.id = r.post_id
+          LEFT JOIN users u
+          ON r.user_id = u.id
+          )
+        ,
+        shares AS (
+          SELECT 
+          r.post_id AS shared_id,
+          COUNT(r.post_id) AS share_count
+          FROM posts p
+          JOIN reposts r
+          ON p.id = r.post_id
+          GROUP BY r.post_id
+          )
+
+        SELECT 
+          t.id, t.user_id, u.image_url, u.name, t.message, t.link,
+          json_build_object(
+            'sharerId', NULL,
+            'shareCount', COALESCE(s.share_count,0),
+            'sharerName', NULL
+          ) AS "shareInfo",
+          CASE WHEN t.repost_date IS NOT NULL
+              THEN t.repost_date
+              ELSE t.date 
+          END AS date,
+          t.user_id = $2 AS "owner"
+
+          FROM tablejoin t
+          LEFT JOIN shares s
+          ON t.id = s.shared_id
+          JOIN users u
+          ON t.user_id = u.id
+		      WHERE t.id IN (SELECT h.post_id FROM hashtags h WHERE h.name = $1)
+          ORDER BY date DESC;
+      
       `,
-        [hashtag]
+        [hashtag, userInfo.user_id]
       );
     } else {
       queryPosts = await connection.query(
